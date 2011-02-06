665202692b52453989848e1b5d61b0ef6eb130fd
Lech Jankovski <godgard+phairy@gmail.com>
2011-02-06 03:46:37 +0200

    Chmoding after copying

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing phairy"
 
-DESTINATION=${1:-"$HOME/xbin"}
+DESTINATION=${1:-"$HOME/bin"}
 
 mkdir -p -v "$DESTINATION"
 if [[ 0 -ne $? ]]; then
@@ -10,10 +10,16 @@ if [[ 0 -ne $? ]]; then
     exit 1
 fi
 
-cp -v src/phairy* "$DESTINATION"
+cp -v "src/phairy"* "$DESTINATION"
 if [[ 0 -ne $? ]]; then
     echo "Something went wrong. Maybe $DESTINATION is not writable?"
     exit 2
+fi
+
+chmod -v a+x "$DESTINATION/phairy"*
+if [[ 0 -ne $? ]]; then
+    echo "Something went wrong, can't make phairy scripts executable."
+    exit 3
 fi
 
 echo "Done!"
